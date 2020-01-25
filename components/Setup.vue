45431b59ac1a7032e791a41670cea201a4b3b631
Kaleb <twitcherc@gmail.com>
2020-01-25 00:27:03 -0700

    store adjustments

--- components/Setup.vue
+++ components/Setup.vue
@@ -2,18 +2,10 @@
   <div>
     <v-text-field filled v-model="host" label="Host"></v-text-field>
     <v-text-field filled v-model="port" type="number" label="Port"></v-text-field>
-    <v-text-field filled
-      v-model="password"
-      label="Password"
-      type="password"
-    ></v-text-field>
+    <v-text-field filled v-model="password" label="Password" type="password"></v-text-field>
     <v-row align="center" align-content="center" justify="center">
       <v-col>
-        <v-text-field
-          outlined=""
-          label="Save as"
-          placeholder="My Server..."
-        ></v-text-field>
+        <v-text-field outlined label="Save as" placeholder="My Server..."></v-text-field>
       </v-col>
       <v-col>
         <v-btn text>Save</v-btn>
@@ -24,8 +16,8 @@
 
     <ul>
       <li>{{host}}</li>
-      <li>{{port}} </li>
-      <li>{{password}} </li>
+      <li>{{port}}</li>
+      <li>{{password}}</li>
     </ul>
   </div>
 </template>
@@ -33,12 +25,17 @@
 <script>
 import { ref, computed } from '@vue/composition-api'
 export default {
-  setup(props, {root: {$store}}) {
+  setup(props, { root: { $store } }) {
     let host = ref('')
     let port = ref('')
     let password = ref('')
-    let rconConnection = computed(() => ({ host: host.value, port: port.value, password: password.value }))
-    const setConnection = () => $store.commit('Console/setConnection', rconConnection.value)
+    let rconConnection = computed(() => ({
+      host: host.value,
+      port: port.value,
+      password: password.value
+    }))
+    const setConnection = () =>
+      $store.commit('console/SetConnection', rconConnection.value)
 
     return { host, port, password, rconConnection, setConnection }
   }
